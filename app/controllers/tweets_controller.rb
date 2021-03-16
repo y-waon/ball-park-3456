6314ee123306091e08b06b98be496d977ee6b28a
y-waon <you0923fs@gmail.com>
2021-03-16 16:48:21 +0900

    ツイート編集・削除機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,28 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
 
   def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :tweets, only:[:new, :create, :show]
+  resources :tweets
   resources :users, only:[:show]
 end
