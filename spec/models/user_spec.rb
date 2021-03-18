8aa199feeeea788bfb2de24de0cf1b90567f00b8
y-waon <you0923fs@gmail.com>
2021-03-18 15:37:49 +0900

    ユーザー登録テストコード再修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,63 +14,63 @@ RSpec.describe User, type: :model do
       it 'nicknameが空だと登録できない' do
         @user.user_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("User name translation missing: ja.activerecord.errors.models.user.attributes.user_name.blank")
+        expect(@user.errors.full_messages).to include("User nameを入力してください")
       end
       it 'emailが空だと登録できない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email translation missing: ja.activerecord.errors.models.user.attributes.email.blank")
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
       it "emailに@を含める必要があるということ" do
         @user.email = "email.com"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email translation missing: ja.activerecord.errors.models.user.attributes.email.invalid")
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
       end
       it "emailが一意性であること" do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email translation missing: ja.activerecord.errors.models.user.attributes.email.taken")
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
       end
       it "passwordが空だと登録できない" do
         @user.password = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password translation missing: ja.activerecord.errors.models.user.attributes.password.blank")
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
       it "パスワードとパスワード（確認用）は、値の一致が必須であること" do
         @user.password = '000a00'
         @user.password_confirmation = '0101a0'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation translation missing: ja.activerecord.errors.models.user.attributes.password_confirmation.confirmation")
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
       it "passwordが6文字以上の入力が必要だということ" do
         @user.password = '0a000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password translation missing: ja.activerecord.errors.models.user.attributes.password.too_short")
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
       end
       it "パスワードは、確認用を含めて2回入力すること" do
         @user.password = '0a0000'
         @user.password_confirmation = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation translation missing: ja.activerecord.errors.models.user.attributes.password_confirmation.confirmation")
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
       it "パスワードは、半角英語のみでは登録できないこと" do
         @user.password = 'aaaaaaa'
         @user.password_confirmation = 'aaaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password translation missing: ja.activerecord.errors.models.user.attributes.password.invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
       it "パスワードは、半角数字のみでは登録できないこと" do
         @user.password = '0000000'
         @user.password_confirmation = '0000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password translation missing: ja.activerecord.errors.models.user.attributes.password.invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
       it "パスワードは、全角英数混合では登録できないこと" do
         @user.password = 'AAAA１１'
         @user.password_confirmation = 'AAAA１１'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password translation missing: ja.activerecord.errors.models.user.attributes.password.invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
     end
   end
