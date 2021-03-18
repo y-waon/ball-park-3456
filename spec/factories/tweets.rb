c29af59f1d7cda09f78955f65e279d379edaa76a
y-waon <you0923fs@gmail.com>
2021-03-18 15:04:10 +0900

    ツイート機能のテストコード

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     status_id    {Faker::Number.between(from: 2, to: 4)}
     category_id  {Faker::Number.between(from: 2, to: 7)}
     area_id      {Faker::Number.between(from: 2, to: 48)}
+    user_id      {1}
     after(:build) do |tweet|
       tweet.image.attach(io: File.open('public/images/bubu.jpeg'), filename: 'bubu.jpeg')
     end
